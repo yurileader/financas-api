cdde63510a70efe6e5b6c2fd9d218a1b0fe8e9c9
Yuri Oliveira <yurileader@gmail.com>
2021-08-22 16:45:49 -0300

    Primeiro commit
    Adicionando as configurações e dependencias iniciais do projeto

---
+++ src/test/java/com/yurileader/financasapi/FinancasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yurileader.financasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancasApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
