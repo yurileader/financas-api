2054776cd2057fff74dddee336141e20c046aa05
Yuri Oliveira <yurileader@gmail.com>
2021-08-26 14:15:58 -0300

    Adicionando entidade Lancamento

--- src/main/java/com/yurileader/financasapi/model/Lancamento.java
+++ src/main/java/com/yurileader/financasapi/model/Lancamento.java
@@ -10,20 +10,25 @@ import java.time.LocalDate;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "lancamento")
 public class Lancamento {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private Long id;
     private String descricao;
+
     @Column(name = "data_vencimento")
-    @JsonFormat(pattern =  "dd/MM/yyyy")
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataVencimento;
+
     @Column(name = "data_pagamento")
-    @JsonFormat(pattern =  "dd/MM/yyyy")
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataPagamento;
     private BigDecimal valor;
     private String observacao;
+
     @Enumerated(EnumType.STRING)
     private TipoLancamento tipo;
 
