eb828b3e5ebb94c5f05625917d41ebf008cfd282
Yuri Oliveira <yurileader@gmail.com>
2021-08-26 15:29:32 -0300

    Adicionando endpoint para salvar Lançamento

--- src/main/java/com/yurileader/financasapi/controller/LancamentoController.java
+++ src/main/java/com/yurileader/financasapi/controller/LancamentoController.java
@@ -1,5 +1,6 @@
 package com.yurileader.financasapi.controller;
 
+import com.yurileader.financasapi.config.event.RecursoCriadoEvent;
 import com.yurileader.financasapi.dto.LancamentoDTO;
 import com.yurileader.financasapi.dto.LancamentoDTOInput;
 import com.yurileader.financasapi.model.Lancamento;
@@ -7,9 +8,11 @@ import com.yurileader.financasapi.service.LancamentoService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -35,21 +38,29 @@ public class LancamentoController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<LancamentoDTO> listarPorId(@PathVariable Long id){
+    public ResponseEntity<LancamentoDTO> listarPorId(@PathVariable Long id) {
         return ResponseEntity.ok(toDto(lancamentoService.buscarPorId(id)));
     }
 
     @PostMapping
-    public ResponseEntity<LancamentoDTOInput> cadastrar (@RequestBody @Valid LancamentoDTOInput lancamentoDTOInput){
-        lancamentoService.criar(lancamentoDTOInput);
+    public ResponseEntity<LancamentoDTOInput> cadastrar(@RequestBody @Valid LancamentoDTOInput lancamentoDTOInput, HttpServletResponse response) {
+        Lancamento lancamentoSalvo = lancamentoService.criar(toEntity(lancamentoDTOInput));
+        publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getId()));
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(toDtoInput(lancamentoSalvo));
     }
 
     private LancamentoDTO toDto(Lancamento lancamento) {
         return modelMapper.map(lancamento, LancamentoDTO.class);
     }
-    private List<LancamentoDTO> toDtoList(List<Lancamento> lancamentos){
+    private LancamentoDTOInput toDtoInput(Lancamento lancamento) {
+        return modelMapper.map(lancamento, LancamentoDTOInput.class);
+    }
+    private Lancamento toEntity(LancamentoDTOInput lancamentoDTOInput) {
+        return modelMapper.map(lancamentoDTOInput, Lancamento.class);
+    }
+    private List<LancamentoDTO> toDtoList(List<Lancamento> lancamentos) {
         return lancamentos.stream().map(lancamento -> toDto(lancamento))
                 .collect(Collectors.toList());
     }
-
 }
