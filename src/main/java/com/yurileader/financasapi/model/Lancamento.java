224254fb62f505be98e979948c3e98328d194e3c
Yuri Oliveira <yurileader@gmail.com>
2021-08-28 15:29:30 -0300

    Adicionando validações em lançamento

--- src/main/java/com/yurileader/financasapi/model/Lancamento.java
+++ src/main/java/com/yurileader/financasapi/model/Lancamento.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
@@ -21,21 +22,28 @@ public class Lancamento {
 
     @Column(name = "data_vencimento")
     @JsonFormat(pattern = "dd/MM/yyyy")
+    @NotNull
     private LocalDate dataVencimento;
 
     @Column(name = "data_pagamento")
     @JsonFormat(pattern = "dd/MM/yyyy")
+    @NotNull
     private LocalDate dataPagamento;
+    @NotNull
     private BigDecimal valor;
     private String observacao;
 
     @Enumerated(EnumType.STRING)
+    @NotNull
     private TipoLancamento tipo;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "fk_id_categoria")
+    @NotNull
     private Categoria categoria;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "fk_id_pessoa")
+    @NotNull
     private Pessoa pessoa;
 }
